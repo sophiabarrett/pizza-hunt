121681e038223e67710b2126d67886327ab66b16
Sophia Barrett <sophia@yoursummit.media>
2022-01-10 11:56:09 -0700

    add additional crud controllers for pizza

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -18,7 +18,7 @@ const pizzaController = {
         if (!dbPizzaData) {
           res
             .status(404)
-            .json({ message: `No pizza found with id ${params.id}` });
+            .json({ message: `No pizza found with id ${params.id}!` });
           return;
         }
       })
@@ -26,6 +26,43 @@ const pizzaController = {
         console.log(err);
         res.status(400).json(err);
       });
+  },
+
+  // create a pizza
+  createPizza({ body }, res) {
+    Pizza.create(body)
+      .then((dbPizzaData) => res.json(dbPizzaData))
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // update pizza
+  updatePizza({ params, body }, res) {
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true }) // { new: true } instructs mongoose to return the updated version of the document
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res
+            .status(404)
+            .json({ message: `No pizza found with id ${params.id}!` });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // delete pizza
+  deletePizza({ params }, res) {
+    Pizza.findOneAndDelete({ _id: params.id })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res
+            .status(404)
+            .json({ message: `No pizza found with id ${params.id}!` });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.status(400).json(err));
   },
 };
 
