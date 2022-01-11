6c2fd930f3f4eb14da83e3d40626a1a6f563a667
Sophia Barrett <sophia@yoursummit.media>
2022-01-10 19:02:22 -0700

    connect Comment model to Pizza model

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,26 +1,45 @@
 const { Schema, model } = require("mongoose");
 
 // create the pizza schema
-const PizzaSchema = new Schema({
-  pizzaName: {
-    type: String,
-  },
-  createdBy: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  size: {
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
       type: String,
-      default: 'Large'
+    },
+    createdBy: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    size: {
+      type: String,
+      default: "Large",
+    },
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
   },
-  toppings: []
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.length;
 });
 
 // create the pizza model
-const Pizza = model('Pizza', PizzaSchema)
+const Pizza = model("Pizza", PizzaSchema);
 
 // export the pizza model
 module.exports = Pizza;
