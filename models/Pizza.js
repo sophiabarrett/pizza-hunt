3078afb39929b3cee510868297a69bbce028baff
Sophia Barrett <sophia@yoursummit.media>
2022-01-10 19:58:19 -0700

    format dates with getter

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
 // create the pizza schema
 const PizzaSchema = new Schema(
@@ -12,6 +13,7 @@ const PizzaSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
     },
     size: {
       type: String,
@@ -28,6 +30,7 @@ const PizzaSchema = new Schema(
   {
     toJSON: {
       virtuals: true,
+      getters: true,
     },
     id: false,
   }
